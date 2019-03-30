b8f3e564aafcd07702b4fa531dfc146ca155b0d6
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-03-30 17:58:03 +0100

    ESLint hinzugefügt

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import Phaser from "phaser";
-import logoImg from "../assets/logo.png";
+import Phaser from 'phaser';
+import logoImg from '../assets/logo.png';
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
+  parent: 'phaser-example',
   width: 800,
   height: 600,
-  backgroundColor: "#CCCCCC"
+  backgroundColor: '#CCCCCC',
 };
 
+// eslint-disable-next-line
 new Phaser.Game(config);
