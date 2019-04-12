b1c46349555a69e0084eb218140b9bda5d0e1df3
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-04-12 19:07:56 +0200

    Die Kraft wird nun korrekt auf "zwei" Räder angewendet

--- src/components/BodyComponent.ts
+++ src/components/BodyComponent.ts
@@ -7,6 +7,6 @@ export default class BodyComponent implements Component {
   public body: Phaser.Physics.Matter.Matter.Body;
 
   public constructor(pos = new Phaser.Math.Vector2()) {
-    this.body = Phaser.Physics.Matter.Matter.Bodies.circle(pos.x, pos.y, 20);
+    this.body = Phaser.Physics.Matter.Matter.Bodies.rectangle(pos.x, pos.y, 20, 20);
   }
 }
--- src/systems/MoveSystem.ts
+++ src/systems/MoveSystem.ts
@@ -15,19 +15,31 @@ export default class MoveSystem implements System {
     all.forEach(entity => {
       const body = entity.getComponent(ComponentType.BODY) as BodyComponent;
       const motor = entity.getComponent(ComponentType.MOTOR) as MotorComponent;
-      const distance = 17;
 
-      // So funtioniert das nicht richtig. Vielleicht Composites benutzen?
-      Phaser.Physics.Matter.Matter.Body.applyForce(
-        body.body,
-        { x: body.body.position.x + distance, y: body.body.position.y },
-        { x: 0, y: 0.002 },
-      );
-      Phaser.Physics.Matter.Matter.Body.applyForce(
-        body.body,
-        { x: body.body.position.x - distance, y: body.body.position.y },
-        { x: 0, y: 0.1 },
-      );
+      const distance = motor.wheelbase;
+
+      // Wir müssen die "Position der Räder" am Vehikel berechnen
+      const offsetRight = Phaser.Physics.Matter.Matter.Vector.rotate({ x: distance, y: 0 }, body.body.angle);
+      const offsetLeft = Phaser.Physics.Matter.Matter.Vector.rotate({ x: -distance, y: 0 }, body.body.angle);
+      // Es muss auch die Richtung "nach vorne" berechnet werden, da das Vehikel eine Rotation
+      // haben kann.
+      const forceX = Phaser.Physics.Matter.Matter.Vector.rotate({ x: 0, y: 0.000000002 }, body.body.angle);
+      const forceY = Phaser.Physics.Matter.Matter.Vector.rotate({ x: 0, y: 0.00001 }, body.body.angle);
+
+      MoveSystem.applyForce(body.body, offsetRight, forceX);
+      MoveSystem.applyForce(body.body, offsetLeft, forceY);
     });
+  }
+
+  private static applyForce(
+    body: Phaser.Physics.Matter.Matter.Body,
+    offset: Phaser.Physics.Matter.Matter.Vector,
+    force: Phaser.Physics.Matter.Matter.Vector,
+  ): void {
+    Phaser.Physics.Matter.Matter.Body.applyForce(
+      body,
+      { x: body.position.x + offset.x, y: body.position.y + offset.y },
+      force,
+    );
   }
 }
