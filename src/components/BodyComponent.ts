29523ef9df8deac148e92a209f23574c865302a6
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-04-12 19:41:38 +0200

    Die Größe von den Entitäten etwas erhöht

--- src/components/BodyComponent.ts
+++ src/components/BodyComponent.ts
@@ -6,7 +6,7 @@ export default class BodyComponent implements Component {
 
   public body: Phaser.Physics.Matter.Matter.Body;
 
-  public size = 20;
+  public size = 50;
 
   public constructor(pos = new Phaser.Math.Vector2()) {
     this.body = Phaser.Physics.Matter.Matter.Bodies.rectangle(pos.x, pos.y, this.size, this.size);
--- src/systems/MoveSystem.ts
+++ src/systems/MoveSystem.ts
@@ -23,8 +23,8 @@ export default class MoveSystem implements System {
       const offsetLeft = Phaser.Physics.Matter.Matter.Vector.rotate({ x: -distance, y: 0 }, body.body.angle);
       // Es muss auch die Richtung "nach vorne" berechnet werden, da das Vehikel eine Rotation
       // haben kann.
-      const forceX = Phaser.Physics.Matter.Matter.Vector.rotate({ x: 0, y: 0.000000002 }, body.body.angle);
-      const forceY = Phaser.Physics.Matter.Matter.Vector.rotate({ x: 0, y: 0.00001 }, body.body.angle);
+      const forceX = Phaser.Physics.Matter.Matter.Vector.rotate({ x: 0, y: 0.0000002 }, body.body.angle);
+      const forceY = Phaser.Physics.Matter.Matter.Vector.rotate({ x: 0, y: 0.001 }, body.body.angle);
 
       MoveSystem.applyForce(body.body, offsetRight, forceX);
       MoveSystem.applyForce(body.body, offsetLeft, forceY);
