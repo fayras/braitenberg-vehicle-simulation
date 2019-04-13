933a1a54d07237030c5507abe9f9637c52ae5385
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-04-13 12:24:11 +0200

    Die Sensor-Komponente hat nun ein Attribut `activation`. Das bestimmt, ob der Sensor aktiv ist oder nicht

--- src/components/SensorComponent.ts
+++ src/components/SensorComponent.ts
@@ -3,11 +3,13 @@ import ComponentType from './types';
 export default class SensorComponent implements Component {
   public name: ComponentType = ComponentType.SENSOR;
 
-  private range: number;
+  public range: number;
 
-  private angle: number;
+  public angle: number;
 
   public position: Phaser.Physics.Matter.Matter.Vector;
+
+  public activation: number = 0.0;
 
   public constructor(offsetPos: Phaser.Physics.Matter.Matter.Vector, range: number, angle: number) {
     this.position = offsetPos;
