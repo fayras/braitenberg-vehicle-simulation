b2b3240bee06e61d1ba4cea8bd5fdf183b5f0e56
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-04-13 12:26:25 +0200

    Sensor-System und -Komponenten zur Szene hinzugefügt

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -8,6 +8,7 @@ import RenderComponent from '../components/RenderComponent';
 import RenderSystem from '../systems/RenderSystem';
 import MotorComponent from '../components/MotorComponent';
 import SensorComponent from '../components/SensorComponent';
+import SensorSystem from '../systems/SensorSystem';
 
 export default class MainScene extends Phaser.Scene {
   private systems: System[] = [];
@@ -27,8 +28,9 @@ export default class MainScene extends Phaser.Scene {
 
     for (let i = 0; i < 1; i += 1) {
       const entity = new Entity();
-      entity.addComponent(new BodyComponent(new Phaser.Math.Vector2(400, 200)));
+      entity.addComponent(new BodyComponent(new Phaser.Math.Vector2(300, 300)));
       entity.addComponent(new RenderComponent('logo'));
+      entity.addComponent(new SensorComponent({ x: 0, y: -35 }, 10, 0.5));
       this.entities.push(entity);
     }
 
@@ -36,7 +38,7 @@ export default class MainScene extends Phaser.Scene {
     entity2.addComponent(new BodyComponent(new Phaser.Math.Vector2(300, 50)));
     entity2.addComponent(new RenderComponent('logo'));
     entity2.addComponent(new MotorComponent());
-    entity2.addComponent(new SensorComponent({ x: 0, y: 50 }, 30, 0.5));
+    entity2.addComponent(new SensorComponent({ x: 0, y: 57 }, 15, 0.5));
     this.entities.push(entity2);
   }
 
