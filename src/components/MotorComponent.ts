38abbbd9b342d28895714311962664a39a064eba
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2019-04-26 10:37:22 +0200

    Motoren brauchen einen relativen Offset am Vehikel.

--- src/components/MotorComponent.ts
+++ src/components/MotorComponent.ts
@@ -3,11 +3,14 @@ import { ComponentType } from '../enums';
 export default class MotorComponent implements Component {
   public name: ComponentType = ComponentType.MOTOR;
 
+  public position: Phaser.Physics.Matter.Matter.Vector;
+
   public defaultSpeed: number;
 
   public maxSpeed: number;
 
-  public constructor(speed: number = 50, defaultSpeed: number = 0) {
+  public constructor(pos: Phaser.Physics.Matter.Matter.Vector, speed: number = 50, defaultSpeed: number = 0) {
+    this.position = pos;
     this.maxSpeed = speed;
     this.defaultSpeed = defaultSpeed;
   }
