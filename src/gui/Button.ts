0aa0d8b4082ae1e9f2fcd17ffbc79a4829fb7c35
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-05-25 00:19:35 +0200

    Die Position der Buttons kann jetzt gesetzt werden.
    Co-authored-by: FraThie <franziska.thiemann@rub.de>

--- src/gui/Button.ts
+++ src/gui/Button.ts
@@ -19,13 +19,23 @@ export default class Button {
       this.handleClick(klickaction);
     });
 
-    this.text = scene.add.text(x, y, text);
-    this.text.x = this.text.x - this.text.displayWidth / 2;
-    this.text.y = this.text.y - this.text.displayHeight / 2;
+    this.text = scene.add.text(x, y, '');
+    this.setText(text);
   }
 
   protected handleClick(klickaction: (btn: Button) => void): void {
     klickaction(this);
-    console.log('geklickt');
+  }
+
+  public setPosition(x: number, y: number): void {
+    this.button.setPosition(x, y);
+    this.text.setPosition(x, y);
+    this.setText(this.text.text);
+  }
+
+  public setText(text: string): void {
+    this.text.setText(text);
+    this.text.x = this.text.x - this.text.displayWidth / 2;
+    this.text.y = this.text.y - this.text.displayHeight / 2;
   }
 }
