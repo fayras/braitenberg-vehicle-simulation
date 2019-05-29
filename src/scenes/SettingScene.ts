78dfa1b70400de890d0dd5f812eabb5b15599837
FraThie <franziska.thiemann@rub.de>
2019-05-29 12:25:36 +0200

    Setting Scene mit Text

--- src/scenes/SettingScene.ts
+++ src/scenes/SettingScene.ts
@@ -1,19 +1,23 @@
 import Phaser from 'phaser';
+import windowImg from '../../assets/gui_window.png';
 
 export default class SettingScene extends Phaser.Scene {
-  //public text: Phaser.GameObjects.Text = 'Test';
+  public text: Phaser.GameObjects.Text = new Text();
 
   public constructor() {
     super({ key: 'SettingScene' });
   }
 
-  public preload(): void {}
+  public preload(): void {
+    this.load.image('gui-window', windowImg);
+  }
 
   public create(): void {
     const rect = new Phaser.Geom.Rectangle(350, 100, 300, 300);
 
     const graphics = this.add.graphics({ fillStyle: { color: 0xffbf00 } });
     graphics.fillRectShape(rect);
-    //const text = this.scene.add.text(100, 100, 'Anzahl Sensoren');
+
+    this.text = this.add.text(500, 130, 'Anzahl Sensoren');
   }
 }
