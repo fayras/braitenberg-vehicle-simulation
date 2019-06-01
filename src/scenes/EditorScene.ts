9e25acd8e0609c0f807e71a37cfac084a4eb95df
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-06-01 17:55:13 +0200

    Expirementell kann man jetzt Objekte in der Sidebar verschieben

--- src/scenes/EditorScene.ts
+++ src/scenes/EditorScene.ts
@@ -1,25 +1,42 @@
 import Phaser from 'phaser';
-import windowImg from '../../assets/gui_window.png';
 import tankImg from '../../assets/tank.png';
 
 export default class EditorScene extends Phaser.Scene {
   // public editor: Phaser.GameObjects.Image;
+  private dragObject: Phaser.GameObjects.GameObject | null = null;
 
   public constructor() {
     super({ key: 'EditorScene' });
   }
 
   public preload(): void {
-    this.load.image('gui-window', windowImg);
     this.load.image('tank', tankImg);
   }
 
   public create(data): void {
-    const rect = new Phaser.Geom.Rectangle(570, 10, 220, 580);
+    const rect = new Phaser.Geom.Rectangle(640, 10, 150, 580);
 
     const graphics = this.add.graphics({ fillStyle: { color: 0xcaff70 } });
     graphics.fillRectShape(rect);
 
-    this.add.image(580, 50, 'tank').setOrigin(0);
+    const tank = this.add
+      .image(675, 30, 'tank')
+      .setOrigin(0)
+      .setInteractive();
+
+    const tank2 = this.add
+      .image(675, 130, 'tank')
+      .setOrigin(0)
+      .setInteractive();
+
+    this.input.setDraggable(tank);
+    this.input.setDraggable(tank2);
+    this.input.on('dragstart', (pointer, gameObject) => {
+      // TODO
+    });
+    this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
+      gameObject.x = dragX;
+      gameObject.y = dragY;
+    });
   }
 }
