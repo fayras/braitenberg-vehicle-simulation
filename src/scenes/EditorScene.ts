a1310d8902a3599e6d066fb198d8ecff0f54113e
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-06-02 13:43:58 +0200

    Der Hintergrund des Editors passt sich nun auch in der Größe an.

--- src/scenes/EditorScene.ts
+++ src/scenes/EditorScene.ts
@@ -5,6 +5,10 @@ export default class EditorScene extends Phaser.Scene {
   // public editor: Phaser.GameObjects.Image;
   private dragObject: Phaser.GameObjects.GameObject | null = null;
 
+  private container: Phaser.GameObjects.Container | null = null;
+
+  private background: Phaser.GameObjects.Graphics | null = null;
+
   public constructor() {
     super({ key: 'EditorScene' });
   }
@@ -13,27 +17,28 @@ export default class EditorScene extends Phaser.Scene {
     this.load.image('tank', tankImg);
   }
 
+  public getWidth(): number {
+    return Math.max(150, this.cameras.main.displayWidth / 6);
+  }
+
   public create(): void {
     this.scale.on('resize', this.handleResize.bind(this));
-    const rect = new Phaser.Geom.Rectangle(
-      this.cameras.main.displayWidth - 200,
-      0,
-      this.cameras.main.displayWidth / 6,
-      this.cameras.main.displayHeight,
-    );
 
-    const graphics = this.add.graphics({ fillStyle: { color: 0xcaff70 } });
-    graphics.fillRectShape(rect);
+    const container = this.add.container(this.cameras.main.displayWidth - this.getWidth(), 0);
+    this.container = container;
 
-    const tank = this.add
-      .image(this.cameras.main.displayWidth - 150, 30, 'tank')
-      .setOrigin(0)
-      .setInteractive();
+    const rect = new Phaser.Geom.Rectangle(0, 0, this.getWidth(), this.cameras.main.displayHeight);
+    this.background = this.add.graphics({ fillStyle: { color: 0xcaff70 } });
+    this.background.fillRectShape(rect);
 
-    const tank2 = this.add
-      .image(this.cameras.main.displayWidth - 150, 130, 'tank')
-      .setOrigin(0)
-      .setInteractive();
+    container.add(this.background);
+
+    const tank = this.add.image(this.getWidth() / 2, 70, 'tank').setInteractive();
+
+    const tank2 = this.add.image(this.getWidth() / 2, 170, 'tank').setInteractive();
+
+    container.add(tank);
+    container.add(tank2);
 
     this.input.setDraggable(tank);
     this.input.setDraggable(tank2);
@@ -61,5 +66,12 @@ export default class EditorScene extends Phaser.Scene {
     const { width, height } = gameSize;
 
     this.cameras.resize(width, height);
+    if (this.container) {
+      this.container.setPosition(this.cameras.main.displayWidth - this.getWidth(), 0);
+    }
+    if (this.background) {
+      const rect = new Phaser.Geom.Rectangle(0, 0, this.getWidth(), this.cameras.main.displayHeight);
+      this.background.fillRectShape(rect);
+    }
   }
 }
