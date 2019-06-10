e0039c473d1716293f337bdf9f32367d8190c66c
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-06-10 19:57:14 +0200

    Bug behoben, bei dem der ResizeHandler gefuert wurde, obwohl die Szene nicht existierte

--- src/scenes/SidebarScene.ts
+++ src/scenes/SidebarScene.ts
@@ -20,16 +20,15 @@ export default abstract class SidebarScene extends Phaser.Scene {
   protected abstract onCreate(container: Phaser.GameObjects.Container, ...args: any): void;
 
   public create(...args: unknown[]): void {
-    this.scale.on('resize', this.handleResize.bind(this));
+    const handler = this.handleResize.bind(this);
+    this.scale.on('resize', handler);
 
     const container = this.add.container(this.cameras.main.displayWidth, 0);
     this.container = container;
 
-    const rect = new Phaser.Geom.Rectangle(0, 0, SidebarScene.getWidth(), this.cameras.main.displayHeight);
     this.background = this.add.graphics({ fillStyle: { color: 0xf8f8f8 } });
-    this.background.fillRectShape(rect);
-    this.background.lineStyle(1, 0xe0e0e0);
-    this.background.strokeLineShape(new Phaser.Geom.Line(0, 0, 0, this.cameras.main.displayHeight));
+    this.fillBackground();
+
     container.add(this.background);
 
     const close = new Button(this, -35, 35, 0, () => {
@@ -39,7 +38,10 @@ export default abstract class SidebarScene extends Phaser.Scene {
         y: 0,
         duration: 100,
         ease: 'Expo.easeInOut',
-        onComplete: () => this.scene.stop(this.key),
+        onComplete: () => {
+          this.scale.off('resize', handler);
+          this.scene.stop(this.key);
+        },
       });
     });
     container.add(close);
@@ -55,6 +57,15 @@ export default abstract class SidebarScene extends Phaser.Scene {
     });
   }
 
+  private fillBackground(): void {
+    if (this.background) {
+      const rect = new Phaser.Geom.Rectangle(0, 0, SidebarScene.getWidth(), this.cameras.main.displayHeight);
+      this.background.fillRectShape(rect);
+      this.background.lineStyle(1, 0xe0e0e0);
+      this.background.strokeLineShape(new Phaser.Geom.Line(0, 0, 0, this.cameras.main.displayHeight));
+    }
+  }
+
   private handleResize(gameSize: Phaser.Structs.Size): void {
     const { width, height } = gameSize;
 
@@ -62,9 +73,6 @@ export default abstract class SidebarScene extends Phaser.Scene {
     if (this.container) {
       this.container.setPosition(this.cameras.main.displayWidth - SidebarScene.getWidth(), 0);
     }
-    if (this.background) {
-      const rect = new Phaser.Geom.Rectangle(0, 0, SidebarScene.getWidth(), this.cameras.main.displayHeight);
-      this.background.fillRectShape(rect);
-    }
+    this.fillBackground();
   }
 }
