92b2ca76d958e9c93a9d4c86bc516db7795e4ed5
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-06-10 22:05:28 +0200

    UI für Sensoren hinzugefügt

--- src/scenes/SettingScene.ts
+++ src/scenes/SettingScene.ts
@@ -3,6 +3,7 @@ import { get, set } from 'lodash-es';
 import Entity from '../Entity';
 import { ComponentType } from '../enums';
 import SidebarScene from './SidebarScene';
+import Component from '../components/Component';
 
 export default class SettingScene extends SidebarScene {
   public constructor() {
@@ -10,33 +11,21 @@ export default class SettingScene extends SidebarScene {
   }
 
   public preload(): void {
-    this.load.html('slider', 'assets/templates/motor.html');
+    this.load.html('motor', 'assets/templates/motor.html');
+    this.load.html('sensor', 'assets/templates/sensor.html');
   }
 
   public onCreate(container: Phaser.GameObjects.Container, entity: Entity): void {
     const uiElements = entity.getAllComponents().map(component => {
       if (component.name === ComponentType.MOTOR) {
-        const sliderElement = this.add.dom(0, 0).createFromCache('slider');
+        const sliderElement = this.add.dom(0, 0).createFromCache('motor');
+        SettingScene.bind(sliderElement, component);
+        return sliderElement;
+      }
 
-        const children = sliderElement.node.querySelectorAll('*');
-        children.forEach(child => {
-          const bind = child.getAttribute('component-bind');
-          if (bind) {
-            const element = child as HTMLInputElement;
-            element.value = get(component, bind);
-          }
-        });
-
-        sliderElement.addListener('change');
-        sliderElement.on('change', (event: { target: HTMLInputElement }) => {
-          console.log('change', event);
-          const t = event.target;
-          const bind = t.getAttribute('component-bind');
-          if (bind) {
-            set(component, bind, t.value);
-          }
-        });
-
+      if (component.name === ComponentType.SENSOR) {
+        const sliderElement = this.add.dom(0, 0).createFromCache('sensor');
+        SettingScene.bind(sliderElement, component);
         return sliderElement;
       }
 
@@ -44,5 +33,26 @@ export default class SettingScene extends SidebarScene {
     });
 
     this.pack(uiElements);
+  }
+
+  private static bind(element: Phaser.GameObjects.DOMElement, component: Component): void {
+    const children = element.node.querySelectorAll('*');
+    children.forEach(child => {
+      const bind = child.getAttribute('component-bind');
+      if (bind) {
+        const el = child as HTMLInputElement;
+        el.value = get(component, bind);
+      }
+    });
+
+    element.addListener('change');
+    element.on('change', (event: { target: HTMLInputElement }) => {
+      console.log('change', event);
+      const t = event.target;
+      const bind = t.getAttribute('component-bind');
+      if (bind) {
+        set(component, bind, t.value);
+      }
+    });
   }
 }
