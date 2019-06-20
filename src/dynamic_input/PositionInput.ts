3af4545c9834dfa45b92921cf5b8753ac5a76847
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-06-20 18:00:30 +0200

    2D-Grid für PositionInput hinzugefügt

--- src/dynamic_input/PositionInput.ts
+++ src/dynamic_input/PositionInput.ts
@@ -1,48 +1,76 @@
+import interact from 'interactjs';
 import BaseInput from './BaseInput';
 
 export default class PositionInput extends BaseInput<Vector2D> {
-  private inputElementX: HTMLInputElement | null = null;
-
-  private inputElementY: HTMLInputElement | null = null;
+  protected position: Vector2D = { x: 0, y: 0 };
 
   protected create(): Element {
-    const container = document.createElement('div');
-    const inputX = document.createElement('input');
-    const inputY = document.createElement('input');
+    this.position = { x: this.value.x + 50, y: this.value.y + 50 };
 
-    inputX.type = 'number';
-    inputY.type = 'number';
-    inputX.step = '0.01';
-    inputY.step = '0.01';
-    inputX.style.display = 'block';
-    inputY.style.display = 'block';
-    inputX.value = (this.value.x as unknown) as string;
-    inputY.value = (this.value.y as unknown) as string;
-    inputX.addEventListener('change', () => {
-      this.value.x = (inputX.value as unknown) as number;
+    const html = `
+      <div class="position-background">
+        <div class="draggable position-indicator"></div>
+      </div>
+    `;
+    const nodes = new DOMParser().parseFromString(html.trim(), 'text/html');
+    const dragEl = nodes.querySelector('.position-indicator') as HTMLDivElement;
+    dragEl.style.transform = `translate(${this.position.x}px, ${this.position.y}px)`;
+
+    interact(dragEl).draggable({
+      listeners: {
+        move: event => {
+          const { target } = event;
+          this.position.x += event.dx;
+          this.position.y += event.dy;
+
+          target.style.transform = `translate(${this.position.x}px, ${this.position.y}px)`;
+        },
+      },
+      modifiers: [
+        interact.modifiers.restrict({
+          restriction: 'parent',
+        }),
+        interact.modifiers.snap({
+          targets: [interact.snappers.grid({ x: 10, y: 10 })],
+          relativePoints: [{ x: 0.5, y: 0.5 }],
+        }),
+      ],
     });
-    inputY.addEventListener('change', () => {
-      this.value.y = (inputY.value as unknown) as number;
-    });
 
-    setTimeout(() => {
-      this.inputElementX = inputX;
-      this.inputElementY = inputY;
-    }, 0);
+    // inputX.type = 'number';
+    // inputY.type = 'number';
+    // inputX.step = '0.01';
+    // inputY.step = '0.01';
+    // inputX.style.display = 'block';
+    // inputY.style.display = 'block';
+    // inputX.value = (this.value.x as unknown) as string;
+    // inputY.value = (this.value.y as unknown) as string;
+    // inputX.addEventListener('change', () => {
+    //   this.value.x = (inputX.value as unknown) as number;
+    // });
+    // inputY.addEventListener('change', () => {
+    //   this.value.y = (inputY.value as unknown) as number;
+    // });
 
-    container.appendChild(inputX);
-    container.appendChild(inputY);
+    // setTimeout(() => {
+    //   this.inputElementX = inputX;
+    //   this.inputElementY = inputY;
+    // }, 0);
 
-    return container;
+    // container.appendChild(inputX);
+    // container.appendChild(inputY);
+    // container.appendChild(dragEl);
+
+    return nodes.body.childNodes[0] as Element;
   }
 
   protected onUpdate(): void {
-    if (this.inputElementX) {
-      this.inputElementX.value = this.value.x.toFixed(2);
-    }
-    if (this.inputElementY) {
-      this.inputElementY.value = this.value.y.toFixed(2);
-    }
+    // if (this.inputElementX) {
+    //   this.inputElementX.value = this.value.x.toFixed(2);
+    // }
+    // if (this.inputElementY) {
+    //   this.inputElementY.value = this.value.y.toFixed(2);
+    // }
   }
 
   // preUpdate(time, delta) {
