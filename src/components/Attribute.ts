f01502b1a94facafa387d31bfe27ae01acafe3f4
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-06-20 17:59:35 +0200

    Es können nun EvenHandler für Änderungen an Attribute angehangen werden

--- src/components/Attribute.ts
+++ src/components/Attribute.ts
@@ -11,6 +11,8 @@ export default class Attribute<T, S extends BaseInput<T>> implements Settable<T>
 
   private el: S | null = null;
 
+  private changeHanlers: ((value: T) => void)[] = [];
+
   public constructor(value: T, label: string, renderAs: ElementConstructor<T, S>) {
     this.value = value;
     this.label = label;
@@ -22,16 +24,32 @@ export default class Attribute<T, S extends BaseInput<T>> implements Settable<T>
   }
 
   public set(value: T): void {
+    const oldValue = this.value;
+
     this.value = value;
+
     if (this.el) {
       this.el.value = value;
+    }
+
+    if (value !== oldValue) {
+      this.changeHanlers.forEach(handler => handler(value));
+    }
+  }
+
+  public onChange(handler: (value: T) => void): void {
+    this.changeHanlers.push(handler);
+  }
+
+  public removeHandler(handler: (value: T) => void): void {
+    const found = this.changeHanlers.findIndex(h => h === handler);
+    if (found) {
+      this.changeHanlers.splice(found, 1);
     }
   }
 
   public render(scene: Phaser.Scene): S {
     this.el = new this.Element(scene, this, this.value, this.label);
-
-    // this.el.onChange(value => this.set(value));
 
     return this.el;
   }
