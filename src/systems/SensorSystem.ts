95cba777b09a7766d20a882d26b023af2408ec23
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-06-23 15:33:33 +0200

    `SensorSystem` erzeugt nun auch keine Physics-Objekte mehr, sondern emittet nur die Werte

--- src/systems/SensorSystem.ts
+++ src/systems/SensorSystem.ts
@@ -12,19 +12,11 @@ import { gaussian } from '../utils/reactions';
 export default class SensorSystem extends System {
   public expectedComponents: ComponentType[] = [ComponentType.SENSOR, ComponentType.TRANSFORMABLE];
 
-  private physicsObjects: {
+  private textures: {
     [componentId: number]: {
-      body: SensorPhysicsObject;
-      eventBeforeUpdate: Function;
+      [angle: string]: Phaser.GameObjects.Image;
     };
   } = {};
-
-  public constructor(scene: Phaser.Scene) {
-    super(scene);
-    // this.scene.matter.world.on('collisionstart', SensorSystem.onCollision);
-    this.scene.matter.world.on('collisionactive', SensorSystem.onCollision);
-    this.scene.matter.world.on('collisionend', SensorSystem.onCollisionEnd);
-  }
 
   public update(): void {}
 
@@ -39,217 +31,88 @@ export default class SensorSystem extends System {
   protected onEntityDestroyed(entity: Entity): void {
     const sensors = entity.getMultipleComponents(ComponentType.SENSOR) as SensorComponent[];
     sensors.forEach(sensor => {
-      const { body, eventBeforeUpdate } = this.physicsObjects[sensor.id];
-
-      this.scene.matter.world.off('beforeupdate', eventBeforeUpdate);
-      this.scene.matter.world.remove(body, false);
-      delete this.physicsObjects[entity.id];
+      const angleTextures = this.textures[sensor.id];
+      Object.keys(angleTextures).forEach(key => {
+        angleTextures[key].destroy();
+      });
+      delete this.textures[sensor.id];
     });
+
+    const found = this.entities.findIndex(e => e === entity);
+    if (found) {
+      this.entities.splice(found, 1);
+    }
   }
 
-  private addSensorObject(entity: Entity, sensor: SensorComponent): SensorPhysicsObject {
-    const body = SensorSystem.createSensor(sensor) as SensorPhysicsObject;
-    const emitter = this.attachSynchronization(body, entity, sensor);
+  private addSensorObject(entity: Entity, sensor: SensorComponent): void {
+    const transform = entity.getComponent(ComponentType.TRANSFORMABLE) as TransformableComponent;
 
     const halfWidth = Math.ceil((sensor.range.get() * 2) / CORRELATION_SCALE);
     const halfHeight = Math.ceil((sensor.range.get() * 2) / CORRELATION_SCALE);
     const width = halfWidth * 2;
     const height = halfHeight * 2;
 
-    const offScreenCanvas = document.createElement('canvas');
-    offScreenCanvas.width = width;
-    offScreenCanvas.height = height;
-    const context = offScreenCanvas.getContext('2d') as CanvasRenderingContext2D;
+    const angleDelta = Math.PI / 2; // 45 Grad
+    const angles = [];
+    for (let a = 0; a < Math.PI * 2; a += angleDelta) {
+      angles.push(a);
+    }
 
-    const offScreenCanvasDown = document.createElement('canvas');
-    offScreenCanvasDown.width = width;
-    offScreenCanvasDown.height = height;
-    const contextDown = offScreenCanvasDown.getContext('2d') as CanvasRenderingContext2D;
-
-    const valuesUp = new Float32Array(width * height);
-    const valuesRight = new Float32Array(width * height);
-    const valuesDown = new Float32Array(width * height);
-    const valuesLeft = new Float32Array(width * height);
     const gauss = gaussian({ x: 0, y: 0 }, { x: sensor.angle.get(), y: sensor.range.get() });
     const f = (x: number, y: number, rotation: number = 0): number => {
       // `atan2` ist für x = 0 und y = 0 nicht definiert.
       if (x === 0 && y === 0) return 1;
 
-      const r = Math.sqrt(x ** 2 + y ** 2);
-      const phi = Math.atan2(x, y);
-      return gauss(phi + rotation, r);
+      const newX = Math.cos(-rotation) * x - Math.sin(-rotation) * y;
+      const newY = Math.sin(-rotation) * x + Math.cos(-rotation) * y;
+
+      const r = Math.sqrt(newX ** 2 + newY ** 2);
+      const phi = Math.atan2(newX, newY);
+      return gauss(phi, r);
     };
 
-    for (let y = 0; y < height; y += 1) {
-      for (let x = 0; x < width; x += 1) {
-        let vUp = f((x - halfWidth) * CORRELATION_SCALE, (y - halfHeight) * CORRELATION_SCALE);
-        let vRight = f((x - halfWidth) * CORRELATION_SCALE, (y - halfHeight) * CORRELATION_SCALE, Math.PI / 2);
+    const textures: { [angle: string]: Phaser.GameObjects.Image } = {};
+    const values: { [angle: string]: Float32Array } = {};
+    angles.forEach(angle => {
+      const texture = this.scene.textures.createCanvas(`sensor_texture_${sensor.id}_${angle}`, width, height);
+      const context = texture.getContext();
+      const angleValues = new Float32Array(width * height);
 
-        // x und y werden hier vertauscht, was einer Spiegelung gleichkommt.
-        // Weil wir später die Cross-Correlation und nicht die Convolution
-        // berechnen wollen.
-        const mirrorX = width - x - 1;
-        const mirrorY = height - y - 1;
+      for (let y = 0; y < height; y += 1) {
+        for (let x = 0; x < width; x += 1) {
+          let v = f((x - halfWidth) * CORRELATION_SCALE, (y - halfHeight) * CORRELATION_SCALE, angle);
 
-        valuesUp[y * width + x] = vUp;
-        valuesRight[y * width + x] = vRight;
-        valuesDown[mirrorY * width + mirrorX] = vUp;
-        valuesLeft[mirrorY * width + mirrorX] = vRight;
+          angleValues[y * width + x] = v;
 
-        vUp = Math.round(vUp * 255);
-        context.fillStyle = `rgb(${vUp}, ${vUp}, ${vUp})`;
-        context.fillRect(x, y, 1, 1);
-        vRight = Math.round(vRight * 255);
-        contextDown.fillStyle = `rgb(${vRight}, ${vRight}, ${vRight})`;
-        contextDown.fillRect(x, y, 1, 1);
+          v = Math.round(v * 255);
+          context.fillStyle = `rgb(${v}, ${0}, ${0})`;
+          context.fillRect(x, y, 1, 1);
+        }
       }
-    }
+
+      // window.open(offScreenCanvas.toDataURL(), '_blank');
+      // window.open(offScreenCanvasDown.toDataURL(), '_blank');
+
+      texture.refresh();
+
+      const x = transform.position.get().x + sensor.position.get().x;
+      const y = transform.position.get().y + sensor.position.get().y;
+      const image = this.scene.add.image(x, y, `sensor_texture_${sensor.id}_${angle}`);
+      image.setScale(CORRELATION_SCALE);
+      image.setBlendMode(Phaser.BlendModes.SCREEN);
+      image.setVisible(false);
+
+      textures[angle] = image;
+      values[angle] = angleValues;
+    });
 
     // window.open(offScreenCanvas.toDataURL(), '_blank');
-    // window.open(offScreenCanvasDown.toDataURL(), '_blank');
 
-    const up = tf.tensor4d(valuesDown, [height, width, 1, 1]);
-    const right = tf.tensor4d(valuesLeft, [height, width, 1, 1]);
-    const down = tf.tensor4d(valuesUp, [height, width, 1, 1]);
-    const left = tf.tensor4d(valuesRight, [height, width, 1, 1]);
-
-    body.label = ComponentType.SENSOR;
-    body.userData = {
-      kernel: f,
-      tensors: [
-        {
-          angle: Math.PI,
-          tensor: up,
-        },
-        {
-          angle: Math.PI + Math.PI / 2,
-          tensor: right,
-        },
-        {
-          angle: 0,
-          tensor: down,
-        },
-        {
-          angle: Math.PI / 2,
-          tensor: left,
-        },
-      ],
-      belongsTo: {
-        entity,
-        // TODO: Hier wird jetzt die Referenz auf die gesamte Komponente gespeichert.
-        // Wäre es besser hier nur die ID zu speichern und dann später mittels sowas
-        // wie `EntityManager` die Komponente über die ID zu holen?
-        component: sensor,
-      },
-    };
-    this.scene.matter.world.add(body);
-    this.physicsObjects[sensor.id] = {
-      body,
-      eventBeforeUpdate: emitter,
-    };
-
-    return body;
-  }
-
-  private attachSynchronization(
-    body: Phaser.Physics.Matter.Matter.Body,
-    entity: Entity,
-    component: SensorComponent,
-  ): Function {
-    const transform = entity.getComponent(ComponentType.TRANSFORMABLE) as TransformableComponent;
-
-    const onBefore = (): void => {
-      component.activation.set(0, true);
-      const direction = Phaser.Physics.Matter.Matter.Vector.rotate(component.position.get(), transform.angle.get());
-      Phaser.Physics.Matter.Matter.Body.setPosition(body, {
-        x: transform.position.get().x + direction.x,
-        y: transform.position.get().y + direction.y,
-      });
-      Phaser.Physics.Matter.Matter.Body.setAngle(body, transform.angle.get());
-    };
-
-    // Hier brauchen wir nur `beforeupdate` (Kein `afterupdate`), da die Position und Winkel
-    // des Sensors immer relativ zum Objekt selbst sind und somit nicht durch die Physik-
-    // Engine beeinflusst werden.
-    this.scene.matter.world.on('beforeupdate', onBefore);
-
-    return onBefore;
-  }
-
-  private static createSensor(component: SensorComponent): Phaser.Physics.Matter.Matter.Body {
-    const width = Math.ceil(component.range.get() * 2);
-    const height = Math.ceil(component.range.get() * 2);
-
-    const direction = Phaser.Physics.Matter.Matter.Vector.normalise(component.position.get());
-    // const fullVector = Phaser.Physics.Matter.Matter.Vector.mult(direction, component.range);
-
-    const body = Phaser.Physics.Matter.Matter.Bodies.rectangle(0, 0, width, height, {
-      isSensor: true,
-    });
-
-    // const offset = Phaser.Physics.Matter.Matter.Vertices.centre(body.vertices);
-
-    // Hier muss die Position gesetzt werden, was ein Workaround
-    // dafür ist Origin-Punkt des Dreiecks zu setzen.
-    // body.positionPrev.x -= height / 2;
-    body.positionPrev.y -= height / 2 - 20;
-    // body.position.x -= height / 2;
-    body.position.y -= height / 2 - 20;
-
-    return body;
-  }
-
-  private static getCollisionBodies(pair: Phaser.Physics.Matter.Matter.IPair): CollisionBodies | null {
-    // Sensoren können nicht mit anderen Sensoren reagieren.
-    if (pair.bodyA.label === ComponentType.SENSOR && pair.bodyB.label === ComponentType.SENSOR) {
-      return null;
-    }
-
-    if (pair.bodyA.label === ComponentType.SENSOR) {
-      return {
-        sensor: pair.bodyA as SensorPhysicsObject,
-        other: pair.bodyB as ComponentPhysicsBody,
-      };
-    }
-
-    if (pair.bodyB.label === ComponentType.SENSOR) {
-      return {
-        sensor: pair.bodyB as SensorPhysicsObject,
-        other: pair.bodyA as ComponentPhysicsBody,
-      };
-    }
-
-    return null;
-  }
-
-  public static onCollision(event: Phaser.Physics.Matter.Events.CollisionActiveEvent): void {
-    event.pairs.forEach(pair => {
-      if (!pair.isSensor) return;
-
-      const bodies = SensorSystem.getCollisionBodies(pair);
-      if (bodies) {
-        // const s = bodies.sensor;
-        // const o = bodies.other;
-        // const dX = o.position.x - s.position.x;
-        // const dY = o.position.y - s.position.y;
-
-        EventBus.publish(EventType.REACTION, bodies);
-
-        // const result = s.userData.kernel(dX, dY);
-        // console.log(result);
-        // bodies.sensor.userData.belongsTo.component.activation = result;
-      }
-    });
-  }
-
-  public static onCollisionEnd(event: Phaser.Physics.Matter.Events.CollisionStartEvent): void {
-    event.pairs.forEach(pair => {
-      if (!pair.isSensor) return;
-
-      const bodies = SensorSystem.getCollisionBodies(pair);
-      if (bodies) {
-        bodies.sensor.userData.belongsTo.component.activation.set(0);
-      }
+    EventBus.publish(EventType.SENSOR_CREATED, {
+      id: sensor.id,
+      values,
+      width,
+      height,
     });
   }
 }
