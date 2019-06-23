c054aa47f09773e848d246c7a15d6d2cb544dee8
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-06-23 15:34:16 +0200

    Im `SourceSystem` werden nun die Entities hinzugefügt und entfernt

--- src/systems/SourceSystem.ts
+++ src/systems/SourceSystem.ts
@@ -23,6 +23,7 @@ export default class SourceSystem extends System {
     sources.forEach(source => {
       this.addSourceObject(entity, source);
     });
+    this.entities.push(entity);
   }
 
   protected onEntityDestroyed(entity: Entity): void {
@@ -31,6 +32,11 @@ export default class SourceSystem extends System {
       this.textures[source.id].destroy();
       delete this.textures[source.id];
     });
+
+    const found = this.entities.findIndex(e => e === entity);
+    if (found) {
+      this.entities.splice(found, 1);
+    }
   }
 
   private addSourceObject(entity: Entity, source: SourceComponent): void {
