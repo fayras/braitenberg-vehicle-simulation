87261ae4dcf962d8dc6d5cec076a95dc6d87bbef
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-07-09 00:05:24 +0200

    Input für die Rotation des Vehikels hinzugefügt

--- src/components/TransformableComponent.ts
+++ src/components/TransformableComponent.ts
@@ -3,18 +3,19 @@ import Component from './Component';
 import Attribute from './Attribute';
 import NumberInput from '../dynamic_input/NumberInput';
 import ReadonlyPosition from '../dynamic_input/ReadonlyPosition';
+import RotationInput from '../dynamic_input/RotationInput';
 
 export default class TransformableComponent extends Component {
   public name: ComponentType = ComponentType.TRANSFORMABLE;
 
   public position: Attribute<Vector2D, ReadonlyPosition>;
 
-  public angle: Attribute<number, NumberInput>;
+  public angle: Attribute<number, RotationInput>;
 
   public constructor(pos: Vector2D = { x: 0, y: 0 }) {
     super();
     this.position = new Attribute(pos, 'Position', ReadonlyPosition);
-    this.angle = new Attribute(0 as number, 'Winkel', NumberInput);
+    this.angle = new Attribute(0 as number, 'Rotation', RotationInput);
   }
 
   public serializeAttributes(): object {
