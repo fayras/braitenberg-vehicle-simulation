a581aef78c0716cffd5eeb9453968cea3c939167
FraThie <franziska.thiemann@rub.de>
2019-08-01 16:18:14 +0200

    Funktionierende Prefabs angefangen

--- src/scenes/EditorScene.ts
+++ src/scenes/EditorScene.ts
@@ -1,7 +1,16 @@
 import Phaser from 'phaser';
+import Entity from '../Entity';
 import EntityManager from '../EntityManager';
-import TransformableComponent from '../components/TransformableComponent';
+import { SubstanceType, EmissionType, BodyShape } from '../enums';
+
+import SolidBodyComponent from '../components/SolidBodyComponent';
 import RenderComponent from '../components/RenderComponent';
+import MotorComponent from '../components/MotorComponent';
+import SensorComponent from '../components/SensorComponent';
+import SourceComponent from '../components/SourceComponent';
+import TransformableComponent from '../components/TransformableComponent';
+import ConnectionComponent from '../components/ConnectionComponent';
+
 import SidebarScene from './SidebarScene';
 
 type DropHandler = (position: { x: number; y: number }) => void;
@@ -24,6 +33,407 @@ export default class EditorScene extends SidebarScene {
         new TransformableComponent({ position }),
         new RenderComponent({ asset: 'prefab-blank', size: 100 }),
       );
+    });
+
+    // Fügt der Standard Quelle die erwarteten Eigenschaften hinzu
+    this.makeInteractable(prefabSource, position => {
+      EntityManager.createEntity(
+        new TransformableComponent({ position }),
+        new SourceComponent({
+          range: 100,
+        }),
+        new RenderComponent({ asset: 'prefab-source', size: 100 }),
+      );
+    });
+
+    // Fügt der Standard Quelle die erwarteten Eigenschaften hinzu
+    this.makeInteractable(prefabSource, position => {
+      EntityManager.createEntity(
+        new TransformableComponent({ position }),
+        new SourceComponent({
+          range: 100,
+        }),
+        new RenderComponent({ asset: 'prefab-source', size: 100 }),
+      );
+    });
+
+    // Fügt das von Braitenberg als Vehikel 2a bezeichnete Gefährt
+    // mit 2 Sensoren und 2 direkt verbundenen Motoren hinzu
+    // Das Vehikel fährt von Lichtquellen weg
+    this.makeInteractable(prefab2a, position => {
+      const entity = new Entity();
+      const transform = new TransformableComponent({
+        position,
+      });
+      transform.angle.set(-Math.PI / 2);
+      entity.addComponent(transform);
+      entity.addComponent(
+        new SolidBodyComponent({
+          size: { width: 100, height: 150 },
+        }),
+      );
+      entity.addComponent(
+        new RenderComponent({
+          asset: 'prefab-2a',
+          size: 100,
+        }),
+      );
+      const motor1 = entity.addComponent(
+        new MotorComponent({
+          position: { x: -50, y: 0 },
+          maxSpeed: 30,
+          defaultSpeed: 1,
+        }),
+      );
+      const motor2 = entity.addComponent(
+        new MotorComponent({
+          position: { x: 50, y: 0 },
+          maxSpeed: 30,
+          defaultSpeed: 1,
+        }),
+      );
+      const sensor1 = entity.addComponent(
+        new SensorComponent({
+          position: { x: -50, y: 75 },
+          range: 20,
+          angle: 0.4,
+        }),
+      );
+      const sensor2 = entity.addComponent(
+        new SensorComponent({
+          position: { x: 50, y: 75 },
+          range: 20,
+          angle: 0.4,
+        }),
+      );
+      const sensor3 = entity.addComponent(
+        new SensorComponent({
+          position: { x: -50, y: 75 },
+          range: 30,
+          angle: 0.4,
+          reactsTo: SubstanceType.BARRIER,
+        }),
+      );
+      const sensor4 = entity.addComponent(
+        new SensorComponent({
+          position: { x: 50, y: 75 },
+          range: 30,
+          angle: 0.4,
+          reactsTo: SubstanceType.BARRIER,
+        }),
+      );
+      entity.addComponent(
+        new ConnectionComponent({
+          inputIds: [sensor1, sensor2, sensor3, sensor4],
+          outputIds: [motor1, motor2],
+          weights: [[1, 0], [0, 1], [0, 1], [1, 0]],
+        }),
+      );
+      EntityManager.addExistingEntity(entity);
+    });
+
+    // Fügt das von Braitenberg als Vehikel 2b bezeichnete Gefährt
+    // mit 2 Sensoren und 2 überkreutz verbundenen Motoren hinzu
+    // Das Gefährt fährt auf Lichtquellen zu
+    this.makeInteractable(prefab2a, position => {
+      const entity = new Entity();
+      const transform = new TransformableComponent({
+        position,
+      });
+      transform.angle.set(-Math.PI / 2);
+      entity.addComponent(transform);
+      entity.addComponent(
+        new SolidBodyComponent({
+          size: { width: 100, height: 150 },
+        }),
+      );
+      entity.addComponent(
+        new RenderComponent({
+          asset: 'prefab-2b',
+          size: 100,
+        }),
+      );
+      const motor1 = entity.addComponent(
+        new MotorComponent({
+          position: { x: -50, y: 0 },
+          maxSpeed: 30,
+          defaultSpeed: 1,
+        }),
+      );
+      const motor2 = entity.addComponent(
+        new MotorComponent({
+          position: { x: 50, y: 0 },
+          maxSpeed: 30,
+          defaultSpeed: 1,
+        }),
+      );
+      const sensor1 = entity.addComponent(
+        new SensorComponent({
+          position: { x: -50, y: 75 },
+          range: 20,
+          angle: 0.4,
+        }),
+      );
+      const sensor2 = entity.addComponent(
+        new SensorComponent({
+          position: { x: 50, y: 75 },
+          range: 20,
+          angle: 0.4,
+        }),
+      );
+      const sensor3 = entity.addComponent(
+        new SensorComponent({
+          position: { x: -50, y: 75 },
+          range: 30,
+          angle: 0.4,
+          reactsTo: SubstanceType.BARRIER,
+        }),
+      );
+      const sensor4 = entity.addComponent(
+        new SensorComponent({
+          position: { x: 50, y: 75 },
+          range: 30,
+          angle: 0.4,
+          reactsTo: SubstanceType.BARRIER,
+        }),
+      );
+      entity.addComponent(
+        new ConnectionComponent({
+          inputIds: [sensor1, sensor2, sensor3, sensor4],
+          outputIds: [motor1, motor2],
+          weights: [[0, 1], [1, 0], [1, 0], [0, 1]],
+        }),
+      );
+      EntityManager.addExistingEntity(entity);
+    });
+
+    // Fügt das von Braitenberg als Vehikel 3a bezeichnete Gefährt
+    // mit 2 Sensoren und 2 verbundenen Motoren hinzu,
+    // die Motoren sind negativ verschaltet
+    // Das Gefährt fährt auf langsam auf die Lichtquellen zu
+    this.makeInteractable(prefab3a, position => {
+      const entity = new Entity();
+      const transform = new TransformableComponent({
+        position,
+      });
+      transform.angle.set(-Math.PI / 2);
+      entity.addComponent(transform);
+      entity.addComponent(
+        new SolidBodyComponent({
+          size: { width: 100, height: 150 },
+        }),
+      );
+      entity.addComponent(
+        new RenderComponent({
+          asset: 'prefab-3a',
+          size: 100,
+        }),
+      );
+      const motor1 = entity.addComponent(
+        new MotorComponent({
+          position: { x: -50, y: 0 },
+          maxSpeed: 30,
+          defaultSpeed: 1,
+        }),
+      );
+      const motor2 = entity.addComponent(
+        new MotorComponent({
+          position: { x: 50, y: 0 },
+          maxSpeed: 30,
+          defaultSpeed: 1,
+        }),
+      );
+      const sensor1 = entity.addComponent(
+        new SensorComponent({
+          position: { x: -50, y: 75 },
+          range: 20,
+          angle: 0.4,
+        }),
+      );
+      const sensor2 = entity.addComponent(
+        new SensorComponent({
+          position: { x: 50, y: 75 },
+          range: 20,
+          angle: 0.4,
+        }),
+      );
+      const sensor3 = entity.addComponent(
+        new SensorComponent({
+          position: { x: -50, y: 75 },
+          range: 30,
+          angle: 0.4,
+          reactsTo: SubstanceType.BARRIER,
+        }),
+      );
+      const sensor4 = entity.addComponent(
+        new SensorComponent({
+          position: { x: 50, y: 75 },
+          range: 30,
+          angle: 0.4,
+          reactsTo: SubstanceType.BARRIER,
+        }),
+      );
+      entity.addComponent(
+        new ConnectionComponent({
+          inputIds: [sensor1, sensor2, sensor3, sensor4],
+          outputIds: [motor1, motor2],
+          weights: [[-1, 0], [0, -1], [0, -1], [-1, 0]],
+        }),
+      );
+      EntityManager.addExistingEntity(entity);
+    });
+
+    // Fügt das von Braitenberg als Vehikel 3a bezeichnete Gefährt
+    // mit 2 Sensoren und 2 verbundenen Motoren hinzu,
+    // die Motoren sind negativ verschaltet
+    // Das Gefährt fährt auf langsam auf die Lichtquellen zu
+    this.makeInteractable(prefab3a, position => {
+      const entity = new Entity();
+      const transform = new TransformableComponent({
+        position,
+      });
+      transform.angle.set(-Math.PI / 2);
+      entity.addComponent(transform);
+      entity.addComponent(
+        new SolidBodyComponent({
+          size: { width: 100, height: 150 },
+        }),
+      );
+      entity.addComponent(
+        new RenderComponent({
+          asset: 'prefab-3a',
+          size: 100,
+        }),
+      );
+      const motor1 = entity.addComponent(
+        new MotorComponent({
+          position: { x: -50, y: 0 },
+          maxSpeed: 30,
+          defaultSpeed: 1,
+        }),
+      );
+      const motor2 = entity.addComponent(
+        new MotorComponent({
+          position: { x: 50, y: 0 },
+          maxSpeed: 30,
+          defaultSpeed: 1,
+        }),
+      );
+      const sensor1 = entity.addComponent(
+        new SensorComponent({
+          position: { x: -50, y: 75 },
+          range: 20,
+          angle: 0.4,
+        }),
+      );
+      const sensor2 = entity.addComponent(
+        new SensorComponent({
+          position: { x: 50, y: 75 },
+          range: 20,
+          angle: 0.4,
+        }),
+      );
+      const sensor3 = entity.addComponent(
+        new SensorComponent({
+          position: { x: -50, y: 75 },
+          range: 30,
+          angle: 0.4,
+          reactsTo: SubstanceType.BARRIER,
+        }),
+      );
+      const sensor4 = entity.addComponent(
+        new SensorComponent({
+          position: { x: 50, y: 75 },
+          range: 30,
+          angle: 0.4,
+          reactsTo: SubstanceType.BARRIER,
+        }),
+      );
+      entity.addComponent(
+        new ConnectionComponent({
+          inputIds: [sensor1, sensor2, sensor3, sensor4],
+          outputIds: [motor1, motor2],
+          //TO-DO Negative Verknüpfung umsetzen
+          weights: [[0, 1], [1, 0], [1, 0], [0, 1]],
+        }),
+      );
+      EntityManager.addExistingEntity(entity);
+    });
+
+    // Fügt das von Braitenberg als Vehikel 3b bezeichnete Gefährt
+    // mit 2 Sensoren und 2 überkreutzten Motoren hinzu,
+    // die Motoren sind negativ verschaltet
+    this.makeInteractable(prefab3b, position => {
+      const entity = new Entity();
+      const transform = new TransformableComponent({
+        position,
+      });
+      transform.angle.set(-Math.PI / 2);
+      entity.addComponent(transform);
+      entity.addComponent(
+        new SolidBodyComponent({
+          size: { width: 100, height: 150 },
+        }),
+      );
+      entity.addComponent(
+        new RenderComponent({
+          asset: 'prefab-3b',
+          size: 100,
+        }),
+      );
+      const motor1 = entity.addComponent(
+        new MotorComponent({
+          position: { x: -50, y: 0 },
+          maxSpeed: 30,
+          defaultSpeed: 1,
+        }),
+      );
+      const motor2 = entity.addComponent(
+        new MotorComponent({
+          position: { x: 50, y: 0 },
+          maxSpeed: 30,
+          defaultSpeed: 1,
+        }),
+      );
+      const sensor1 = entity.addComponent(
+        new SensorComponent({
+          position: { x: -50, y: 75 },
+          range: 20,
+          angle: 0.4,
+        }),
+      );
+      const sensor2 = entity.addComponent(
+        new SensorComponent({
+          position: { x: 50, y: 75 },
+          range: 20,
+          angle: 0.4,
+        }),
+      );
+      const sensor3 = entity.addComponent(
+        new SensorComponent({
+          position: { x: -50, y: 75 },
+          range: 30,
+          angle: 0.4,
+          reactsTo: SubstanceType.BARRIER,
+        }),
+      );
+      const sensor4 = entity.addComponent(
+        new SensorComponent({
+          position: { x: 50, y: 75 },
+          range: 30,
+          angle: 0.4,
+          reactsTo: SubstanceType.BARRIER,
+        }),
+      );
+      entity.addComponent(
+        new ConnectionComponent({
+          inputIds: [sensor1, sensor2, sensor3, sensor4],
+          outputIds: [motor1, motor2],
+          //TO-DO Negative Verknüpfung umsetzen
+          weights: [[0, 1], [1, 0], [1, 0], [0, 1]],
+        }),
+      );
+      EntityManager.addExistingEntity(entity);
     });
 
     // this.makeInteractable(prefab, position => {
