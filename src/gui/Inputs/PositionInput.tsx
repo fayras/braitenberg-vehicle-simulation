257c319ec2215c317bb65d8f0b718cd2b1a1a755
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2021-09-10 14:53:42 +0200

    PositionInput ist nun funktional und hat ein Styling bekommen

--- src/gui/Inputs/PositionInput.tsx
+++ src/gui/Inputs/PositionInput.tsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import RenderableAttribute from '../../components/RenderableAttribute';
+import { Box, NumberInput, NumberInputField, FormLabel, Flex, Spacer } from '@chakra-ui/react';
 
 type Props = {
-  // value: Vector2D;
   attribute: RenderableAttribute<Vector2D, any>;
   label: string;
 };
 
 export default observer((props: Props): JSX.Element => {
-  // const { test: count, increase } = testStore();
-
   return (
-    <div>
-      {props.label}: {props.attribute.value.x}, {props.attribute.value.y}
-    </div>
+    <Box mb="2.5">
+      <FormLabel color="gray.800" fontSize="sm" mb="0">
+        {props.label}
+      </FormLabel>
+      <Flex>
+        <NumberInput
+          value={props.attribute.value.x}
+          size="sm"
+          variant="filled"
+          onChange={(val) => {
+            props.attribute.set({ x: Number(val), y: props.attribute.value.y });
+          }}
+        >
+          <NumberInputField />
+        </NumberInput>
+        <Spacer mx="1" />
+        <NumberInput
+          value={props.attribute.value.y}
+          size="sm"
+          variant="filled"
+          onChange={(val) => {
+            props.attribute.set({ x: props.attribute.value.x, y: Number(val) });
+          }}
+        >
+          <NumberInputField />
+        </NumberInput>
+      </Flex>
+    </Box>
   );
 });
