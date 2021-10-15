e047be7b21e7f1967870353af2eb5b177f3060fb
Dimitri Tarnavski <dimitri.tarn@gmail.com>
2021-10-15 20:18:24 +0200

    `System` umgeschrieben: implementiert nun das Konzept von Entity Queries.

--- src/systems/System.ts
+++ src/systems/System.ts
@@ -1,83 +1,32 @@
-import { ComponentType, EventType } from '../enums';
-import EventBus from '../EventBus';
 import Entity from '../Entity';
-import Component from '../components/Component';
+import EntityManager, { EntityQuery } from '../EntityManager';
+import { ComponentType } from '../enums';
 
 export default abstract class System {
-  public expectedComponents: ComponentType[] = [];
-
-  protected entities: Entity[] = [];
-
   protected scene: Phaser.Scene;
-
+  protected expectedComponents: ComponentType[];
+  protected canBePaused: boolean;
+  protected query: EntityQuery;
   protected isPaused: boolean = false;
 
-  public constructor(scene: Phaser.Scene) {
+  public constructor(scene: Phaser.Scene, expectedComponents: ComponentType[], canBePaused: boolean = true) {
     this.scene = scene;
-    EventBus.subscribe(EventType.ENTITY_CREATED, (entity: Entity) => {
-      if (entity.hasComponents(...this.expectedComponents)) {
-        this.onEntityCreated(entity);
-        this.entities.push(entity);
-      }
-    });
+    this.expectedComponents = expectedComponents;
+    this.canBePaused = canBePaused;
 
-    EventBus.subscribe(EventType.ENTITY_DESTROYED, (entity: Entity) => {
-      if (entity.hasComponents(...this.expectedComponents)) {
-        this.onEntityDestroyed(entity);
-
-        const found = this.entities.findIndex(e => e.id === entity.id);
-        if (found > -1) {
-          this.entities.splice(found, 1);
-        }
-      }
-    });
-
-    EventBus.subscribe(EventType.ENTITY_COMPONENT_ADDED, (payload: any) => {
-      const entity = payload.entity as Entity;
-      const component = payload.component as Component;
-
-      const hasComponents = entity.hasComponents(...this.expectedComponents);
-      const exists = this.entities.includes(entity);
-
-      if (hasComponents && !exists) {
-        this.entities.push(entity);
-      }
-
-      this.onEntityComponentAdded(entity, component);
-    });
-
-    EventBus.subscribe(EventType.ENTITY_COMPONENT_REMOVED, (payload: any) => {
-      const entity = payload.entity as Entity;
-      const component = payload.component as Component;
-
-      const hasComponents = entity.hasComponents(...this.expectedComponents);
-
-      this.onEntityComponentRemoved(entity, component);
-
-      if (!hasComponents) {
-        const found = this.entities.findIndex(e => e.id === entity.id);
-        if (found > -1) {
-          this.entities.splice(found, 1);
-        }
-      }
-    });
+    this.query = EntityManager.createQuery(this.expectedComponents);
+    // query.onEntityAdded((entity) => this.createPhysicsObject(entity))
   }
 
   public pause(flag: boolean): void {
     this.isPaused = flag;
   }
 
-  public abstract update(delta: number): void;
+  public update(delta: number): void {
+    if (this.canBePaused && this.isPaused) return;
 
-  // eslint-disable-next-line
-  protected onEntityCreated(entity: Entity): void {}
+    this.internalUpdate(this.query.entities, delta);
+  }
 
-  // eslint-disable-next-line
-  protected onEntityDestroyed(entity: Entity): void {}
-
-  // eslint-disable-next-line
-  protected onEntityComponentAdded(entity: Entity, component: Component): void {}
-
-  // eslint-disable-next-line
-  protected onEntityComponentRemoved(entity: Entity, component: Component): void {}
+  protected abstract internalUpdate(entities: ReadonlySet<Entity>, delta: number): void;
 }
